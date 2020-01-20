acdc673d44abdb0f44d6824ce37aba0b536eb488
Kevin Stubblefield <kevin@stubblefield.io>
2020-01-20 00:34:00 -0600

    Create index page. Install axios

---
+++ src/components/Recipes.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="recipes">
+        <article v-for="recipe in recipes" :key="recipe.id">
+            <a :href="'http://localhost:3000/' + recipe.id">{{ recipe.name }}</a>
+        </article>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            recipes: []
+        };
+    },
+    mounted() {
+        axios.get('http://localhost:3000/').then(result => {
+            this.recipes = result.data;
+            console.log(result);
+        }, error => {
+            console.error(error);
+        });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
